49af0b7ecc997759dd2d48c1363c0a22abc20d80
Ronald <rontbrilhante@Gmail.com>
2021-12-23 19:22:12 -0500

    corrected media settings from main url to blog url

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from max_my_site import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.starting_page, name='starting-page'),
     path('posts', views.posts, name='posts-page',),
     path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
