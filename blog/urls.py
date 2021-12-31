c5f23865c74ac7acc2bae105378fcc7c1f0c1a9d
Ronald <rontbrilhante@Gmail.com>
2021-12-31 14:10:45 -0500

    added saved posts page and logic.

--- blog/urls.py
+++ blog/urls.py
@@ -9,10 +9,12 @@ admin.site.site_header = 'Blog Admin - Site Header'
 admin.site.index_title = "Blog - Index Title"
 
 urlpatterns = [
-                  # path('', views.starting_page, name='starting-page'),
-                  path('', views.StartingPageView.as_view(), name='starting-page'),
-                  path('posts', views.PostsView.as_view(), name='posts-page'),
-                  path('posts/<slug:slug>', views.SinglePostDetail.as_view(), 
-                  name='post-detail-page'),
-                  path('read-later', views.ReadLaterView.as_view(), name='read-later'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # path('', views.starting_page, name='starting-page'),
+    path('', views.StartingPageView.as_view(),
+         name='starting-page'),
+    path('posts', views.PostsView.as_view(), name='posts-page'),
+    path('posts/<slug:slug>', views.SinglePostDetail.as_view(),
+         name='post-detail-page'),
+    path('read-later', views.ReadLaterView.as_view(),
+         name='read-later'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -32,13 +32,23 @@ class PostsView(ListView):
 
 class SinglePostDetail(View):
 
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get('stored_posts')
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+
         context = {
             "post": post,
             "post_tags": post.tag.all(),
             "comment_form": CommentForm(),
-            "comments":post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
         return render(request, 'blog/post-detail.html', context)
 
@@ -53,27 +63,44 @@ class SinglePostDetail(View):
             return HttpResponseRedirect(reverse('post-detail-page', args=[slug]))
 
         else:
-            print('invalid form data')
             context = {
                 "post": post,
                 "post_tags": post.tag.all(),
                 "comment_form": comment_form,
-                "comments":post.comments.all().order_by("-id")
+                "comments": post.comments.all().order_by("-id"),
+                "saved_for_later": self.is_stored_post(request, post.id)
+
             }
             return render(request, 'blog/post-detail.html', context)
 
 
-def get_date(post):
-    return post.get("date")
-
 class ReadLaterView(View):
+
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context['posts'] = []
+            context['has_posts'] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context['posts'] = posts
+            context['has_posts'] = True
+
+        return render(request, "blog/stored_post.html", context)
+
     def post(self, request):
         stored_posts = request.session.get("stored_posts")
         post_id = int(request.POST["post_id"])
         if stored_posts is None:
             stored_posts = []
-        
+
         if post_id not in stored_posts:
             stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+
+        request.session['stored_posts'] = stored_posts
 
         return HttpResponseRedirect("/")
